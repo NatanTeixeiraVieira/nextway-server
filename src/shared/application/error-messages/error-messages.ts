a67e9d2e8fb2b72dab1ce9745de7b16a4c89db92
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-12-21 17:34:28 -0300

    feat: Add bad request error and error messages

---
+++ src/shared/application/error-messages/error-messages.ts
@@ -0,0 +1,11 @@
+export const ErrorMessages = {
+	EMAIL_NOT_INFORMED: 'Email não informado',
+	NAME_NOT_INFORMED: 'Nome não informado',
+	PASSWORD_NOT_INFORMED: 'Senha não informada',
+	INVALID_EMAIL: 'Email inválido',
+	INVALID_PASSWORD: 'Senha inválida',
+	INVALID_NAME: 'Nome inválido',
+	INVALID_CPF: 'CPF inválido',
+	INVALID_PHONE: 'Telefone inválido',
+	EMAIL_ALREADY_EXISTS: 'Email já cadastrado',
+};

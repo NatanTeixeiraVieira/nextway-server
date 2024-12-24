05d6c880a31a35ae05edb743b43e3296d875f174
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-12-24 11:04:55 -0300

    feat: Add base schema and typeorm testing config

---
+++ src/shared/infra/database/typeorm/testing/config-typeorm-module-tests.ts
@@ -0,0 +1,40 @@
+import { UserSchema } from '@/core/user/infra/database/typeorm/schemas/user.schema';
+import { EnvConfig } from '@/shared/application/env-config/env-config';
+import { EnvConfigModule } from '@/shared/infra/env-config/env-config.module';
+import { EnvConfigService } from '@/shared/infra/env-config/env-config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { setupDatabase } from '../config';
+
+export function configTypeOrmModule() {
+	const module = TypeOrmModule.forRootAsync({
+		imports: [EnvConfigModule],
+		useFactory: async (envConfig: EnvConfig) => {
+			return {
+				type: 'postgres',
+				host: envConfig.getDbHost(),
+				port: envConfig.getDbPort(),
+				username: envConfig.getDbUsername(),
+				password: envConfig.getDbPassword(),
+				database: envConfig.getDbName(),
+				schema: envConfig.getDbSchema(),
+				entities: [UserSchema],
+				synchronize: true,
+				logging: true,
+			};
+		},
+		inject: [EnvConfigService],
+
+		dataSourceFactory: async (options) => {
+			if (!options) {
+				throw new Error('Invalid options passed');
+			}
+
+			await setupDatabase(options as PostgresConnectionOptions);
+			return new DataSource(options);
+		},
+	});
+
+	return module;
+}
